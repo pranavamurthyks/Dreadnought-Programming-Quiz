7bb918793b9e62f2490726dc564047f0ecb5fea1
Pranavamurthy KS <pranavamurthyks45@gmail.com>
2025-07-20 19:15:25 +0530

    Final Commit

--- bonus_question.py
+++ bonus_question.py
@@ -1,5 +1,19 @@
 # The code was taken from chatgpt and I understood it. It wasn't typed by me from scratch.
 
+'''
+From what I understood, fashion_mnist is a dataset with 60000 images and the type of clothing
+it is. All the images are 28x28 and are black and white. First we load these images into a 
+variable fashion_mnist and are scaled down so computer could learn better.
+After this a nueral model is created which has three steps. One makes the image into a 
+line of numbers, one learns the patterns and one predicts the output.
+
+The model is trained using 60,000 images, and it learns by adjusting itself every time 
+it makes a mistake. After training, it is tested on 10,000 new images to see how well 
+it learned. Finally, the program shows a test image and prints both the actual and 
+predicted labels to see if it got it right.'''
+
+
+
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
